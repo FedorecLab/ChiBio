7ecee0546bd36e1279c009230111158d9d7ecaff
cxen <c_x_@outlook.com>
2026-02-07 06:32:22 +0100

    Fix for missing csv headers (For Labmaker devices only?). Add small guardrails for application initialization logic.

--- app.py
+++ app.py
@@ -1976,8 +1976,9 @@ def csvData(M):
                   'internal_air_temp', 'external_air_temp', 'media_temp', 'opt_gen_act_int', 'pump_1_rate',
                   'pump_2_rate', 'pump_3_rate', 'pump_4_rate', 'media_vol', 'stirring_rate', 'LED_395nm_setpoint',
                   'LED_457nm_setpoint', 'LED_500nm_setpoint', 'LED_523nm_setpoint', 'LED_595nm_setpoint',
-                  'LED_623nm_setpoint', 'LED_6500K_setpoint', 'laser_setpoint', 'LED_UV_int', 'FP1_base', 'FP1_emit1',
-                  'FP1_emit2', 'FP1_emit1_raw', 'FP1_emit2_raw', 'FP2_base', 'FP2_emit1', 'FP2_emit2', 'FP2_emit1_raw',
+                  'LED_623nm_setpoint', 'LED_6500K_setpoint', 'LED_600nm_setpoint', 'LED_550nm_setpoint', 
+                  'LED_White_setpoint', 'laser_setpoint', 'LED_UV_int', 'FP1_base', 'FP1_emit1', 'FP1_emit2', 
+                  'FP1_emit1_raw', 'FP1_emit2_raw', 'FP2_base', 'FP2_emit1', 'FP2_emit2', 'FP2_emit1_raw', 
                   'FP2_emit2_raw', 'FP3_base', 'FP3_emit1', 'FP3_emit2', 'FP3_emit1_raw', 'FP3_emit2_raw',
                   'custom_prog_param1', 'custom_prog_param2', 'custom_prog_param3', 'custom_prog_status',
                   'zigzag_target', 'growth_rate', 'od_raw']
@@ -2467,8 +2468,9 @@ def runExperiment(M, placeholder):
 if __name__ == '__main__':
     initialiseAll()
     application.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
-
-initialiseAll()
+else:
+    initialiseAll()
+    
 info_msg = ' Start Up Complete'
 application.logger.info(info_msg)
 print(str(datetime.now()) + info_msg)
